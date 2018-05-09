ad6fb74f346bf85d298afc2e09bd2fed1d7c4922
wzy <357658949@qq.com>
2018-05-09 15:07:17 +0800

    增加跳转示例

--- TestXGWFramework/ViewController.m
+++ TestXGWFramework/ViewController.m
@@ -13,6 +13,10 @@
 
 @property (nonatomic, strong) UIButton *tradeButton;
 
+@property (nonatomic, strong) UIButton *buyButton;
+
+@property (nonatomic, strong) TradeController *tradeController;
+
 @end
 
 @implementation ViewController
@@ -24,24 +28,36 @@
     self.tradeButton = [[UIButton alloc]init];
     [self.tradeButton setTitle:@"交易" forState:UIControlStateNormal];
     [self.tradeButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    
     [self.tradeButton addTarget:self action:@selector(navigationToTrade) forControlEvents:UIControlEventTouchUpInside];
-    
     [self.view addSubview:self.tradeButton];
+    
+    self.buyButton = [[UIButton alloc]init];
+    [self.buyButton setTitle:@"买入万科A" forState:UIControlStateNormal];
+    [self.buyButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [self.buyButton addTarget:self action:@selector(navigationToBuy) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.buyButton];
+    
+    self.tradeController = [[TradeController alloc]init];
+    //    tradeController.backButtonHidden = YES;//隐藏导航栏的返回按钮
+    self.tradeController.isSecondTrade = YES;
+    self.tradeController.navigationToStockDetailBlock = ^(NSDictionary *params) {
+        NSLog(@"%@",params);
+    };
+}
+
+- (void)navigationToBuy {
+    [self.navigationController pushViewController:self.tradeController animated:YES];
+    [[NSNotificationCenter defaultCenter]postNotificationName:@"RHNavigationToTradeBuy" object:@"sz_000002"];
 }
 
 - (void)navigationToTrade {
-    TradeController *tradeController = [[TradeController alloc]init];
-//    tradeController.backButtonHidden = YES;//隐藏导航栏的返回按钮
-    tradeController.isSecondTrade = YES;
-    tradeController.navigationToStockDetailBlock = ^(NSDictionary *params) {
-        NSLog(@"%@",params);
-    };
-    [self.navigationController pushViewController:tradeController animated:YES];
+    [self.navigationController pushViewController:self.tradeController animated:YES];
 }
 
 - (void)viewWillLayoutSubviews {
-    self.tradeButton.frame = CGRectMake(100, 100, 100, 100);
+    self.tradeButton.frame = CGRectMake(100, 100, 100, 20);
+    
+    self.buyButton.frame = CGRectMake(100, 150, 100, 20);
 }
 
 - (void)didReceiveMemoryWarning {
